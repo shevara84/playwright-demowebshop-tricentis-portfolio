18065c138e58d1181175ba8abe5b08efd437301b
Zoran <zoransevo@gmail.com>
2026-09-08 12:28:46 +0200

    test: add authentication test data

---
+++ pages/login.page.ts
@@ -0,0 +1,28 @@
+import { BasePage } from "@pages/base.page";
+import { Page, Locator } from "@playwright/test";
+
+
+export class LoginPage extends BasePage {
+
+    readonly email: Locator;
+    readonly password: Locator;
+    readonly loginButton: Locator;
+    
+    
+    
+    constructor(page: Page) {
+        super(page);
+        this.email = page.getByRole('textbox', { name: 'Email' });
+        this.password = page.getByRole('textbox', { name: 'Password'});
+        this.loginButton = page.getByRole('button', { name: 'Log in'});
+        
+    }
+
+    //fill up the login form and click login button
+    async logIn(email: string, password: string) { 
+        await this.email.fill(email);
+        await this.password.fill(password);
+        await this.loginButton.click();
+
+    }
+}
